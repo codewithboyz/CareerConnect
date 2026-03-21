08e45f83b0f3e0b52f6aa19bf69fba3d9e88c587
santu509 <sausantu544@gmail.com>
2026-03-21 19:25:16 +0530

    commit by santu

--- landing/nav.php
+++ landing/nav.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="landing/css/nav.css">
-  <nav class="navbar navbar-expand-lg navbar-light custom-navbar py-3 sticky-top"
-    style=" background: rgba(255, 255, 255, 0.6); ">
+
+<nav class="navbar navbar-expand-lg navbar-light custom-navbar py-3 sticky-top"
+  style=" background: rgba(255, 255, 255, 0.6);backdrop-filter: blur(10px); ">
   <div class="container">
 
     <div class="container">
